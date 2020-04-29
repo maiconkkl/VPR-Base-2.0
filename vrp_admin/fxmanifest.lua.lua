cc40e9ed90c51972dffc384b8b5c4187b3eca035
Michael Douglas <contatomichael@outlook.com.br>
2020-04-29 10:42:15 -0300

    vrp_admin
    Migrate VRP Admin

---
+++ vrp_admin/fxmanifest.lua.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game 'gta5'
+description 'VRP Admin'
+version '1.0.0'
+client_scripts {
+	"@vrp/lib/utils.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"@vrp/lib/utils.lua",
+	"server.lua"
+}
---
+++ vrp_admin/server_vrp.lua
@@ -0,0 +1,6 @@
+local Proxy = module("vrp", "lib/Proxy")
+local vRP = Proxy.getInterface("vRP")
+
+async(function()
+  vRP.loadScript("vrp_admin", "server/server")
+end)
